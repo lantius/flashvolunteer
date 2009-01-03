6e6c74c326fdef406882ed792e7314ebabe12b6d
lantatlantius <lantatlantius@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2009-01-03 23:49:28 +0000

    Initial import of a hello world-esque app

---
+++ home.py
@@ -0,0 +1,26 @@
+import cgi
+import wsgiref.handlers
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+		template_values = { 
+		  'foo' : 'bar', 
+			}
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, template_values))
+
+def main():
+	application = webapp.WSGIApplication(
+		                                   [('/', MainPage),
+																				],
+		                                    debug=True)
+	wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+  main()
+
+
