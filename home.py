c1d3986408c7d2c4a05636486c047ec731ebf0f4
lantatlantius <lantatlantius@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2009-01-04 01:08:16 +0000

    Added models

--- home.py
+++ home.py
@@ -2,23 +2,47 @@ import cgi
 import wsgiref.handlers
 import os
 
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-		template_values = { 
-		  'foo' : 'bar', 
-			}
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, template_values))
+    template_values = { 
+      }
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
+################################################################################
+# models
+################################################################################
+class Volunteer(db.Model):
+  user = db.UserProperty()
+  neighborhood = db.StringProperty()
+
+class Event(db.Model):
+  name = db.StringProperty()
+  date = db.DateProperty()
+  neighborhood = db.StringProperty()
+  
+class EventVolunteer(db.Model):
+  event = db.ReferenceProperty(Event,
+                               required = True,
+                               collection_name = 'volunteers')
+  volunteer = db.ReferenceProperty(Volunteer,
+                                    required = True,
+                                    collection_name = 'events')
+  isowner = db.BooleanProperty(required = True)
+
+################################################################################
+# gae mojo
+################################################################################
 def main():
-	application = webapp.WSGIApplication(
-		                                   [('/', MainPage),
-																				],
-		                                    debug=True)
-	wsgiref.handlers.CGIHandler().run(application)
+  logging.getLogger().setLevel(logging.DEBUG)
+  application = webapp.WSGIApplication(
+                                    [('/', MainPage),
+                                    ],
+                                    debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
   main()
