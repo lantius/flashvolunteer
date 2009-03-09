ff98730d9ace79d7bfe739ccf11c1c94200c7d4c
lantatlantius <lantatlantius@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2009-03-09 08:34:27 +0000

    Unit testing for authentication using a recipe from appengine cookbook

--- controllers/_auth.py
+++ controllers/_auth.py
@@ -7,25 +7,28 @@ class Authorize():
   def login(req, requireUser=False, requireVolunteer=False, redirectTo=''):
     user = users.get_current_user()
     
-    if requireUser and not user:
-      req.redirect(users.create_login_url(req.request.uri))
-      sys.exit
+    if requireUser:
+      if not user:
+        req.redirect(users.create_login_url(req.request.uri))
+        sys.exit            # should end execution IRL
+        return (None, None) # shouldn't get here except in tests
     
     if user:
       volunteer = Volunteer.gql("where user = :user", user=user).get()
-      
+    else:
+      volunteer = None
+    
     if requireVolunteer:
       
       if not volunteer:
         req.redirect(redirectTo)
-        raise NoVolunteerError, "No volunteer for the user"
-
-        volunteer = volunteers.get()
-
+        sys.exit            # should end execution IRL
+        return (None, None) # shouldn't get here except in tests
+        
       if volunteer and req.request.method == 'POST' and not volunteer.check_session_id(req.request.get('session_id')):
         req.redirect('/timeout')
-        raise TimeoutError, "Volunteer's session timed out"
-    
+        sys.exit            # should end execution IRL
+        return (None, None) # shouldn't get here except in tests    
     
     return(user,volunteer)
   
---
+++ test/auth_test.py
@@ -0,0 +1,96 @@
+import unittest
+from webtest import TestApp
+from google.appengine.ext import webapp
+
+from controllers._auth import *
+
+# http://appengine-cookbook.appspot.com/recipe/testing-apps-with-authentication/
+class AuthorizeTest(unittest.TestCase):
+  def setUp(self):
+    self.temp_gcu = users.get_current_user
+  
+  def tearDown(self):
+    users.get_current_user = self.temp_gcu
+    
+  def login(self, user):
+    users.get_current_user = lambda user=user : users.User(user) if user else None
+
+  def logout(self):
+    self.login(None)
+
+  #### ugly testing harness idea ####
+  class harness_req():
+    class harness_request():
+
+      def get(self, id):
+        return self.params[id]
+    
+      method = 'GET'
+      uri = '/'
+      
+    request = harness_request()
+    
+    new_url = None
+    
+    def redirect(self, url):
+      self.new_url = url
+  #### ugliness ends here #### 
+  
+  def test_login(self):
+    req = self.harness_req()
+    req.request.method = "GET"
+
+    self.login('test@example.com')
+    
+    (user, volunteer) = Authorize.login(req, requireUser=False, requireVolunteer=False, redirectTo='/test_url')
+    self.assertEqual(user.email(), 'test@example.com')
+
+    volunteer  = Volunteer()
+    volunteer.user = user
+    volunteer.put()
+    
+    (user, volunteer) = Authorize.login(req, requireUser=False, requireVolunteer=False, redirectTo='/test_url')
+    self.assertEqual(user.email(), 'test@example.com')
+    self.assertEqual(volunteer.user.email(), 'test@example.com')
+    
+    self.logout()
+    (user, volunteer) = Authorize.login(req, requireUser=True, requireVolunteer=True, redirectTo='/test_url')
+    
+    self.assertEqual(req.new_url, users.create_login_url('/'))
+
+    self.login('test-2@example.com')
+    (user, volunteer) = Authorize.login(req, requireUser=True, requireVolunteer=True, redirectTo='/test_url')
+    
+    self.assertEqual(req.new_url, '/test_url')
+    
+  def test_authenticated_post(self):  
+    req = self.harness_req()
+    req.request.method = "POST"
+    req.request.params = {'session_id' : '12345'}
+    
+    self.login('test@example.com')
+    user = users.get_current_user()
+    volunteer  = Volunteer()
+    volunteer.user = user
+    volunteer.session_id = '12345'
+    volunteer.put()
+
+    self.assertEqual(req.request.method, 'POST')
+    self.assertEqual(req.request.get('session_id'), '12345')
+    self.assertEqual(volunteer.session_id, '12345')
+    self.assertTrue(volunteer.check_session_id(req.request.get('session_id')))
+    (user, volunteer) = Authorize.login(req, requireUser=True, requireVolunteer=True, redirectTo='/test_url')
+    self.assertEqual(user.email(), 'test@example.com')
+    self.assertEqual(volunteer.user.email(), 'test@example.com')
+    
+    volunteer.session_id = 'abcde'
+    volunteer.put()
+    
+    self.assertEqual(req.request.method, 'POST')
+    self.assertEqual(req.request.get('session_id'), '12345')
+    self.assertEqual(volunteer.session_id, 'abcde')
+    self.assertFalse(volunteer.check_session_id(req.request.get('session_id')))
+    
+    (user, volunteer) = Authorize.login(req, requireUser=True, requireVolunteer=True, redirectTo='/test_url')
+    self.assertEqual(req.new_url, '/timeout')
+    
