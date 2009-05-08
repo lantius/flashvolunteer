f0dc6c42803efa9ecca63be516bfe089c212a90c
tkriplean <tkriplean@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2009-05-08 00:45:11 +0000

    converting test to use framework

--- gui_integration_tests/navigation/org_hit_all_nav.py
+++ gui_integration_tests/navigation/org_hit_all_nav.py
@@ -1,20 +1,12 @@
 from selenium import selenium
 import unittest, time, re, os
+from gui_integration_tests.test_cases import LoginFirst_Organization
 
 
-class NewTest(unittest.TestCase):
-    def setUp(self):
-        self.verificationErrors = []
-        self.selenium = selenium("localhost", 4444, "*chrome", "http://localhost:9999/")
-        self.selenium.start()
-    
+class NewTest(LoginFirst_Organization):
+
     def test_new(self):
         sel = self.selenium
-        sel.open("/")
-        sel.click("//div[@id='buttons']/a[1]/span[2]")
-        sel.wait_for_page_to_load("30000")
-        sel.type("email", "org@example.com")
-        sel.click("submit-login")
         sel.wait_for_page_to_load("30000")
         sel.click("link=EVENTS")
         sel.wait_for_page_to_load("30000")
@@ -27,13 +19,7 @@ class NewTest(unittest.TestCase):
         sel.click("link=HELP")
         sel.wait_for_page_to_load("30000")
         sel.click("link=CREATE EVENT")
-        sel.wait_for_page_to_load("30000")
-        sel.click("link=Log Out")
-        sel.wait_for_page_to_load("30000")
     
-    def tearDown(self):
-        self.selenium.stop()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
