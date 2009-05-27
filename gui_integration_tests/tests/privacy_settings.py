c82d5c065839539665d7e0d6056f13f0ea85b37c
tkriplean <tkriplean@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2009-05-27 07:03:28 +0000

    privacy settings gui tests: refactoring + todos

--- gui_integration_tests/tests/privacy_settings.py
+++ gui_integration_tests/tests/privacy_settings.py
@@ -5,7 +5,45 @@ from gui_integration_tests.datastore_interface import get_events, delete_event
 from selenium import selenium
 import unittest
 
-class TestPrivacySettings(BaseTestCase):
+  ###TODO: different people should have different privacy settings, not all set to "friends" 
+  ###TODO: tests for viewing someone else's profile
+  ###TODO: tests for upcoming event
+  
+class TestPrivacySettingsBase(BaseTestCase):
+    
+    def _see_all_attendees(self):
+        sel = self.selenium
+    
+        sel.click("//a[@id='l_events']")
+        sel.wait_for_page_to_load("30000")
+    
+        sel.click("//a[@id='event_past[%i]']"%self.test_object_index['Sign the Declaration of Independence'].key().id())
+        sel.wait_for_page_to_load("30000")
+        
+        self.failUnless(sel.is_text_present("John Hancock"))    
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id()))  
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Elbridge Gerry'].key().id()))
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Arthur Middleton'].key().id()))    
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Lyman Hall'].key().id()))    
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['William Williams'].key().id()))    
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Matthew Thornton'].key().id()))          
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['John Witherspoon'].key().id()))  
+
+    def _see_only_attendees_user_has_permission_for(self):
+        sel = self.selenium
+    
+        sel.click("//a[@id='l_events']")
+        sel.wait_for_page_to_load("30000")
+    
+        sel.click("//a[@id='event_past[%i]']"%self.test_object_index['Sign the Declaration of Independence'].key().id())
+        sel.wait_for_page_to_load("30000")
+        
+        self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id()))    
+        self.failUnless(sel.is_text_present("John Hancock"))    
+        self.failUnless(not sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['John Hart'].key().id()))    
+
+### for event attender
+class TestPrivacySettings(TestPrivacySettingsBase):
   test_env = TestEnv(
      organization = False,
      create_new_user = False,
@@ -13,87 +51,38 @@ class TestPrivacySettings(BaseTestCase):
      login_name = 'Thomas Jefferson'
      )
 
-    
 
-#  def test_default_environment_privacy_settings__friend_list(self):
-#    
-#    sel = self.selenium
-#    
-#    sel.click("//a[@id='l_friends']")
-#    sel.wait_for_page_to_load("30000")
-#    
-#    self.failUnless(sel.is_text_present("Aaron Burr"))
-#    self.failUnless(sel.is_text_present("Benjamin Harrison"))
-#    self.failUnless(sel.is_text_present("George Wythe"))
-#    self.failUnless(sel.is_text_present("James Madison"))
-#
-#    
-#    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['Aaron Burr'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(not sel.is_text_present("Upcoming Events"))
-#
-#    sel.click("//a[@id='l_friends']")
-#    sel.wait_for_page_to_load("30000")
-#    
-#    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['Benjamin Harrison'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(not sel.is_text_present("Upcoming Events"))
-#
-#    sel.click("//a[@id='l_friends']")
-#    sel.wait_for_page_to_load("30000")
-#
-#    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(sel.is_text_present("Upcoming Events"))
-#
-#    sel.click("//a[@id='l_friends']")
-#    sel.wait_for_page_to_load("30000")
-#
-#    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['James Madison'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(sel.is_text_present("Upcoming Events"))
-#
-#    sel.click("//a[@id='l_friends']")
-#    sel.wait_for_page_to_load("30000")
-#
-#  def test_adding_to_family__friend_list(self):
-#      
-#    sel = self.selenium
-#    
-#    sel.click("//a[@id='l_friends']")
-#    sel.wait_for_page_to_load("30000")
-#    
-#    sel.click("//input[@id='operation_add_to_family[%i]']"%self.test_object_index['Aaron Burr'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(sel.is_element_present("//input[@id='operation_remove_from_family[%i]']"%self.test_object_index['Aaron Burr'].key().id()))    
-#      
-#    sel.click("//input[@id='operation_remove_as_neighbor[%i]']"%self.test_object_index['Benjamin Harrison'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(not sel.is_text_present("Benjamin Harrison"))    
-#      
-#    sel.click("//input[@id='operation_remove_from_family[%i]']"%self.test_object_index['George Wythe'].key().id())
-#    sel.wait_for_page_to_load("30000")
-#    self.failUnless(sel.is_element_present("//input[@id='operation_add_to_family[%i]']"%self.test_object_index['George Wythe'].key().id()))    
-#    
-#    
-  def test_default_environment_privacy_settings__event_list(self):
+  
+  ### tests what an event attender can see about who is attending an upcoming event signing of declaration
+#  def test_event_list_upcoming_attender(self):
+#      self._see_only_attendees_user_has_permission_for()
 
-    sel = self.selenium
+  ### tests what an event attender can see about who attended a past event signing of declaration
+  def test_event_list_past_attender(self):
+      self._see_all_attendees()
+      
 
-    sel.click("//a[@id='l_events']")
-    sel.wait_for_page_to_load("30000")
 
-    sel.click("//a[@id='event_past[%i]']"%self.test_object_index['Sign the Declaration of Independence'].key().id())
-    sel.wait_for_page_to_load("30000")
-    
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id()))    
-    self.failUnless(sel.is_text_present("John Hancock"))    
-    self.failUnless(not sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['John Hart'].key().id()))    
+#### for someone who is not attending the event signing of declaration...
+class TestPrivacySettings__anyone(TestPrivacySettingsBase):
+  test_env = TestEnv(
+     organization = False,
+     create_new_user = True,
+     login_email = 'test@example.com',
+     )
 
-#  def test_changing_privacy_setting(self):
-#      pass
+  ### tests what anyone can see about who is attending an upcoming event signing of declaration 
+#  def test_event_list_upcoming_anyone(self):
+#      self._see_only_attendees_user_has_permission_for()
 
-class TestPrivacySettings_EventCreator(BaseTestCase):
+
+  ### tests what anyone can see about who attended a past event
+  def test_event_list_past_anyone(self):
+      self._see_only_attendees_user_has_permission_for()      
+
+
+### for event creator
+class TestPrivacySettings_EventCreator(TestPrivacySettingsBase):
   test_env = TestEnv(
      organization = True,
      create_new_user = False,
@@ -101,23 +90,11 @@ class TestPrivacySettings_EventCreator(BaseTestCase):
      login_name = 'John Hancock'
      )
   
-  def test_event_creator_should_see_all_attendies(self):
-    sel = self.selenium
+#  def test_event_creator_upcoming(self):
+#    self._see_all_attendees()
 
-    sel.click("//a[@id='l_events']")
-    sel.wait_for_page_to_load("30000")
-
-    sel.click("//a[@id='event_past[%i]']"%self.test_object_index['Sign the Declaration of Independence'].key().id())
-    sel.wait_for_page_to_load("30000")
-    
-    self.failUnless(sel.is_text_present("John Hancock"))    
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id()))  
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Elbridge Gerry'].key().id()))
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Arthur Middleton'].key().id()))    
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Lyman Hall'].key().id()))    
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['William Williams'].key().id()))    
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['Matthew Thornton'].key().id()))          
-    self.failUnless(sel.is_element_present("//a[@id='volunteer_link[%i]']"%self.test_object_index['John Witherspoon'].key().id()))    
+  def test_event_creator_past(self):
+    self._see_all_attendees() 
     
 if __name__ == "__main__":
     unittest.main()
