5574a422516ee7b242cc9b0518fdccdfc5cba281
tkriplean <tkriplean@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2009-06-07 22:14:25 +0000

    (Fixes issue 50) finishing social networking tests

--- gui_integration_tests/tests/social_network_operations.py
+++ gui_integration_tests/tests/social_network_operations.py
@@ -15,65 +15,65 @@ class TestSocialNetworkOperations(BaseTestCase):
 
     
 
-  def test_default_environment_privacy_settings__friend_list(self):
+  def test_social_network_basic(self):
     
     sel = self.selenium
     
     sel.click("//a[@id='l_friends']")
     sel.wait_for_page_to_load("30000")
     
-    self.failUnless(sel.is_text_present("Aaron Burr"))
-    self.failUnless(sel.is_text_present("Benjamin Harrison"))
-    self.failUnless(sel.is_text_present("George Wythe"))
-    self.failUnless(sel.is_text_present("James Madison"))
+    friends = ["George Wythe", "James Madison", "George Washington", "Thomas McKean"]
+    followers = ["Aaron Burr"]
+    following = ["Benjamin Harrison"]
 
+    for friend in friends:
+        self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_family']//a[@id='volunteer_link[%i]']"%self.test_object_index[friend].key().id()))    
+
+    for friend in followers:
+        self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_follower']//a[@id='volunteer_link[%i]']"%self.test_object_index[friend].key().id()))    
+
+    for friend in following:
+        self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_following']//a[@id='volunteer_link[%i]']"%self.test_object_index[friend].key().id()))    
     
-    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['Aaron Burr'].key().id())
-    sel.wait_for_page_to_load("30000")
-    self.failUnless(not sel.is_text_present("Upcoming Events"))
-
-    sel.click("//a[@id='l_friends']")
-    sel.wait_for_page_to_load("30000")
-    
-    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['Benjamin Harrison'].key().id())
-    sel.wait_for_page_to_load("30000")
-    self.failUnless(not sel.is_text_present("Upcoming Events"))
-
-    sel.click("//a[@id='l_friends']")
-    sel.wait_for_page_to_load("30000")
-
-    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id())
-    sel.wait_for_page_to_load("30000")
-    self.failUnless(sel.is_text_present("Upcoming Events"))
-
-    sel.click("//a[@id='l_friends']")
-    sel.wait_for_page_to_load("30000")
-
-    sel.click("//a[@id='volunteer_link[%i]']"%self.test_object_index['James Madison'].key().id())
-    sel.wait_for_page_to_load("30000")
-    self.failUnless(sel.is_text_present("Upcoming Events"))
-
-    sel.click("//a[@id='l_friends']")
-    sel.wait_for_page_to_load("30000")
-
-  def test_adding_to_family__friend_list(self):
-      
-    sel = self.selenium
-    
-    sel.click("//a[@id='l_friends']")
-    sel.wait_for_page_to_load("30000")
     
     sel.click("//input[@id='operation_add_to_family[%i]']"%self.test_object_index['Aaron Burr'].key().id())
     sel.wait_for_page_to_load("30000")
+    self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_family']//a[@id='volunteer_link[%i]']"%self.test_object_index['Aaron Burr'].key().id()))    
     self.failUnless(sel.is_element_present("//input[@id='operation_remove_from_family[%i]']"%self.test_object_index['Aaron Burr'].key().id()))    
+      
       
     sel.click("//input[@id='operation_remove_as_neighbor[%i]']"%self.test_object_index['Benjamin Harrison'].key().id())
     sel.wait_for_page_to_load("30000")
-    self.failUnless(not sel.is_text_present("Benjamin Harrison"))    
-      
+    self.failIf(sel.is_element_present("//div[@class='volunteer_summary_following']//a[@id='volunteer_link[%i]']"%self.test_object_index["Benjamin Harrison"].key().id()))    
+    self.failIf(sel.is_text_present("Benjamin Harrison"))    
+    
+
     sel.click("//input[@id='operation_remove_from_family[%i]']"%self.test_object_index['George Wythe'].key().id())
     sel.wait_for_page_to_load("30000")
+    self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_follower']//a[@id='volunteer_link[%i]']"%self.test_object_index['George Wythe'].key().id()))    
     self.failUnless(sel.is_element_present("//input[@id='operation_add_to_family[%i]']"%self.test_object_index['George Wythe'].key().id()))    
-        
+    
+    sel.open('/volunteers/%i'%self.test_object_index['Benjamin Harrison'].key().id())
+    sel.wait_for_page_to_load('30000')
+    self.failUnless(sel.is_element_present("//input[@id='operation_add_as_neighbor[%i]']"%self.test_object_index['Benjamin Harrison'].key().id()))    
+    sel.click("//input[@id='operation_add_as_neighbor[%i]']"%self.test_object_index['Benjamin Harrison'].key().id())
+    sel.wait_for_page_to_load('30000')
+    self.failUnless(sel.is_element_present("//input[@id='operation_remove_as_neighbor[%i]']"%self.test_object_index['Benjamin Harrison'].key().id()))    
+    sel.open('/team')
+    sel.wait_for_page_to_load('30000')
+    self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_following']//a[@id='volunteer_link[%i]']"%self.test_object_index['Benjamin Harrison'].key().id()))    
+
+    sel.open('/volunteers/%i'%self.test_object_index['James Madison'].key().id())
+    sel.wait_for_page_to_load('30000')
+    self.failUnless(sel.is_element_present("//input[@id='operation_remove_from_family[%i]']"%self.test_object_index['James Madison'].key().id()))    
+    sel.click("//input[@id='operation_remove_from_family[%i]']"%self.test_object_index['James Madison'].key().id())
+    sel.wait_for_page_to_load('30000')
+    self.failUnless(sel.is_element_present("//input[@id='operation_add_to_family[%i]']"%self.test_object_index['James Madison'].key().id()))    
+    sel.open('/team')
+    sel.wait_for_page_to_load('30000')
+    self.failUnless(sel.is_element_present("//div[@class='volunteer_summary_follower']//a[@id='volunteer_link[%i]']"%self.test_object_index['James Madison'].key().id()))    
+
+
+
 if __name__ == "__main__":
     unittest.main()
