f6515ddf43e0a8da431486e5cea8cbbc18d10168
tkriplean <tkriplean@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2010-02-01 00:32:32 +0000

    "helper" link for login; bug fixing; refactoring

--- stylesheets/javascript/fv.js
+++ stylesheets/javascript/fv.js
@@ -3101,3 +3101,25 @@ complete();if(settings(this).fixPNG)helper.parent.unfixPNG();}})(jQuery);
 
 })(jQuery);
 
+function handle_notification_message(notification_message){
+    $('#container').append('<div id="dialog" title="Notifications"><p>'+notification_message+'</p></div>');
+    $.ui.dialog.defaults.bgiframe = true;
+    $(function() {
+        $("#dialog").dialog({
+            buttons: { "Ok": function() { $(this).dialog("close"); } },
+            modal: true,
+            draggable: false,
+            resizable: false,
+        });
+    });
+}
+
+function handle_header_message(header_message){
+	if (header_message){ 
+	    $('#message_banner').html('<p>'+header_message+'</p>');
+	    $('#message_banner').show();
+	}
+	else {
+	   $('#message_banner').hide();
+	};
+}
