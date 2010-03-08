d30b7dbab581cf3e588fc827f86cc92e6b16b92a
tkriplean <tkriplean@f165528c-d9ed-11dd-9aad-3f7dfcff7c77>
2010-03-08 06:51:35 +0000

    misc refactoring

--- components/applications/defs/__init__.py
+++
@@ -1,6 +0,0 @@
-from components.applications.defs.regions import LosAngeles, PierceCounty, Seattle
-from components.applications.defs.georgetown_app import GeorgeTown
-
-#TODO : make the methods of app defs static methods
-regions = [LosAngeles(), PierceCounty(), Seattle()]
-georgetown_app = [GeorgeTown()]
